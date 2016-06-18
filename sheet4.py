072e5bcda8b244c9b31c9b49d19cb7dbf7ff87d7
Amina Echchiki <amina.echchiki@unil.ch>
2016-06-18 14:33:42 +0200

    implement dijkstra

--- sheet4.py
+++ sheet4.py
@@ -72,4 +72,52 @@ def print_result(node,start,pstart,number_nodes, parent):
             print ('{}-'.format(node[p]),)
             p=parent[p]
         print ('{}\n'.format(start)) 
+        
+        
+# Task 2: implement Dijkstra
+
+# input file with network data
+# toy.sif: toy network as in the instruction Figure
+# network_final_sym.sif: Network of the NBT Primer, with all symmetric edges
+
+fileName="toy.sif"
+
+# Set the node from which all shortest paths will be computed
+start='A'
+
+def dijkstra(fileName, start):
+     # Load the network
+    net = load_network(fileName)
+    print (net)
+    number_edges = len(net) # number of edges
+    nodes = get_list_nodes(net)     # get the list of nodes
+    print (nodes)
+    number_nodes = len(nodes) # number of nodes
+    # Create a hash to link a node with its relted position within the node list
+    pos = create_hash_node_idx(nodes)
+    # Init the variable for Dijkstra's algorithm
+    pstart = pos[start] 
+    # Index of the starting node
+    Max=100000 
+    # Maximal distance to prevent too long computation
+    d=0 # distance to the starting node
+    # This is the list of parent nodes (from which a node has been reached)
+    parent = [0]*number_nodes
+    # This is the distance to the starting node
+    dist = [Max]*number_nodes 
+    # This is the list of nodes that have been monitored
+    seen = [] 
+    dist[pstart]=d 
+    seen.append(pstart)
+    while len(seen) < number_nodes: # as long as all nodes have not been visited
+        for i in range(number_edges): # for each edge in the network 
+            if dist[pos[net[i][0]]]==d and dist[pos[net[i][2]]]==Max: # if node_A is at distance of interest + node_B not yet computed
+                dist[pos[net[i][2]]]=d+1 # set to node_B the distance of interest of the next iteration +1 here because vertices not labeled
+                parent[pos[net[i][2]]]=pos[net[i][0]] # set node_A as parent of node_B
+                seen.append(pos[net[i][2]]) # tag node_B as seen
+        d=d+1 # increase the distance to the starting node   
+    print_result(nodes,start,pstart,number_nodes,parent)
+
+# write to output
+export_result('shortest_path_test.txt',node,start,pstart,number_nodes)
 
